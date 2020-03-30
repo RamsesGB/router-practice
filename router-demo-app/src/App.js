b2e97b22940a12e137bef792da56c23077088773
Ramses Bermudez <ramsesbermudez@Ramsess-MacBook-Pro.local>
2020-03-30 18:28:04 -0400

    set up basic routing in app component

--- router-demo-app/src/App.js
+++ router-demo-app/src/App.js
@@ -1,15 +1,51 @@
 import React from 'react';
-
+import { Link, Switch, Route } from "react-router-dom"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
 
-      </header>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
     </div>
   );
+}
+
+function Home() {
+  return <h2>Home Component</h2>;
+}
+
+function About() {
+  return <h2>About Component</h2>;
+}
+
+function Users() {
+  return <h2>Users Component</h2>;
 }
 
 export default App;
